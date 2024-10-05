306166f8448ed507f8ea658813250a60dfcaf33e
hassan <hassanibrahimndahiro@gmail.com>
2024-10-05 16:49:42 +0300

    initial

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // code your solution here
+
+// function razzle(lawyer = 'Billy', target = "'em"){
+// console.log(`${lawyer} razle-dazzles ${target}!`);
+// }
+// razzle();
+// razzle("Methusella", "T'challah");
+
+// function saturdayFun(){
+//     console.log(`This Saturday, I want to ${activity}!`);
+// }
+// console.log(saturdayFun());
+// saturdayFun("bathe my dog");
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const f = function(){
+    console.log("Yet more razzling");
+}
+f();
+
+const mondayWork = function(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+
+console.log(mondayWork());
+
+//function for wrapadjective
+function wrapAdjective(flair = '*'){
+    return function(adjective = "special"){
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+const WrapWithStars = wrapAdjective();
+console.log(WrapWithStars("a hard worker"));
+
+const WrapWithPipes = wrapAdjective("||");
+console.log(WrapWithPipes("a dedicated programmer"));
+
